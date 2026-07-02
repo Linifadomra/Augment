827ba87124ac7cd89d4f45ee99d99ed786b22fa9
lammmab <liamlam8282@gmail.com>
2026-07-01 22:21:54 -0400

    Better guarding & split

--- cmake/AugmentManifest.cmake
+++ cmake/AugmentManifest.cmake
@@ -5,7 +5,7 @@ endif()
 
 function(augment_manifest)
     cmake_parse_arguments(AM "SEPARATE_TARGET;REGENERATE_AST"
-        "TARGET;OUTPUT;COMPILE_COMMANDS;PROJECT_ROOT;REGISTRY_OUT;PCH_OUT;AST_MANIFEST_HINT_DIR"
+        "TARGET;OUTPUT;BINARY;COMPILE_COMMANDS;PROJECT_ROOT;REGISTRY_OUT;PCH_OUT;AST_MANIFEST_HINT_DIR"
         "EXCLUDE;EXCLUDE_PREFIX;EXCLUDE_PATH"
         ${ARGN})
     find_package(Python3 REQUIRED COMPONENTS Interpreter)
@@ -41,20 +41,34 @@ function(augment_manifest)
         endforeach()
     endif()
 
-    set(_target_bin $<TARGET_FILE:${AM_TARGET}>)
+    if(AM_BINARY)
+        set(_target_bin "${AM_BINARY}")
+    else()
+        set(_target_bin $<TARGET_FILE:${AM_TARGET}>)
+    endif()
 
     if(WIN32 AND MSVC)
-        target_compile_options(${AM_TARGET} PRIVATE
-            $<$<NOT:$<OR:$<CONFIG:RelWithDebInfo>,$<CONFIG:Debug>>>:/Zi>
-        )
         set(_extract_src "$<TARGET_PDB_FILE:${AM_TARGET}>")
         set(_fmt_arg "--debug-format" "pdb")
     else()
-        target_compile_options(${AM_TARGET} PRIVATE
-            $<$<NOT:$<OR:$<CONFIG:RelWithDebInfo>,$<CONFIG:Debug>>>:-g>
-        )
         set(_extract_src "${_target_bin}")
         set(_fmt_arg "--debug-format" "dwarf")
+    endif()
+
+    if(TARGET ${AM_TARGET})
+        get_target_property(_target_imported ${AM_TARGET} IMPORTED)
+
+        if(NOT _target_imported)
+            if(WIN32 AND MSVC)
+                target_compile_options(${AM_TARGET} PRIVATE
+                    $<$<NOT:$<OR:$<CONFIG:RelWithDebInfo>,$<CONFIG:Debug>>>:/Zi>
+                )
+            else()
+                target_compile_options(${AM_TARGET} PRIVATE
+                    $<$<NOT:$<OR:$<CONFIG:RelWithDebInfo>,$<CONFIG:Debug>>>:-g>
+                )
+            endif()
+        endif()
     endif()
 
     cmake_path(GET AUGMENT_EXTRACT_SCRIPT PARENT_PATH _extract_script_dir)
